a84a3f14918ce6db68859200f796f96a7bb96d71
Prakash Deep Sharma <prakashdeep2902@gmail.com>
2026-04-14 13:11:24 +0530

    added new comment

--- todo.js
+++ todo.js
@@ -8,40 +8,93 @@ const EditBtn = document.querySelector(".complete-btn");
 function addingATask(e) {
   const value = TaskInput.value.trim();
   if (value === "") return alert("you are not added any task");
+  const data = {
+    key: "task",
+    value: value,
+  };
+  const res = createList(data);
+  if (res === true) {
+    TaskInput.value = "";
+  }
+}
 
-  const li = document.createElement("li");
-
-  li.innerHTML = `
-  <span>${value}</span>
-    <div class="actions">
+const createList = function (data) {
+  const { key, value } = data;
+  if (data) {
+    const ls = document.createElement("li");
+    ls.innerHTML = `
+      <span>${value}</span>
+      <div class="actions">
       <button class="complete-btn">✔</button>
       <button class="delete-btn">✖</button>
-    </div>
-`;
+     </div>
+      `;
 
-  TaskSpace.appendChild(li);
+    TaskSpace.appendChild(ls);
+    const tasks = JSON.parse(localStorage.getItem("task")) || [];
+    let obj = {
+      value: value,
+      status: "pending",
+    };
 
-  localStorage.setItem(value, li);
-  TaskInput.value = "";
-}
+    tasks.push(obj);
+    localStorage.setItem("task", JSON.stringify(tasks));
+
+    return true;
+  }
+};
 
 AddingTaskBtn.addEventListener("click", addingATask);
 
-const username = localStorage.getItem("vikash");
-
-console.log();
-
 TaskSpace.addEventListener("click", (e) => {
   const target = e.target;
-
   if (target.classList.contains("delete-btn")) {
     const li = target.closest("li");
-    console.log(li);
+    const ls = JSON.parse(localStorage.getItem("task"));
+    const removeValue = li.querySelector("span").textContent;
+    const removedarray = ls.filter(({ value }) => value !== removeValue);
+    localStorage.setItem("task", JSON.stringify(removedarray));
     li.remove();
   }
 
   if (target.classList.contains("complete-btn")) {
     const li = target.closest("li");
+    const getName = li.querySelector("span").textContent;
+
+    const LS = JSON.parse(localStorage.getItem("task"));
+
+    const ChangedStatus = LS.map(({ value, status }) => {
+      if (getName.trim() === value) {
+        status = "done";
+      }
+      return { value, status };
+    });
+
+    console.log("ChangedStatus::==>", ChangedStatus);
+    localStorage.setItem("task", JSON.stringify(ChangedStatus));
+
     li.classList.toggle("completed");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const tasks = JSON.parse(localStorage.getItem("task")) || [];
+
+  if (tasks.length > 0) {
+    tasks.map(({ value, status }) => {
+      const ls = document.createElement("li");
+      ls.innerHTML = `
+      <span>${value}</span>
+      <div class="actions">
+      <button class="complete-btn">✔</button>
+      <button class="delete-btn">✖</button>
+     </div>
+      `;
+
+      if (status == "done") {
+        ls.classList.toggle("completed");
+      }
+      TaskSpace.appendChild(ls);
+    });
   }
 });
